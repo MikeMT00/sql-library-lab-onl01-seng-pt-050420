38cba3aece5e3c3556de52788ed8f5ed9247c13e
Michael Thomas <michaelthomas0611@gmail.com>
2020-07-05 10:54:42 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,15 @@
 INSERT INTO series (id, title) VALUES (1, 'Fresh Prince'), (2, 'Bernie Mac');
+
 INSERT INTO subgenres (id, name) VALUES (1, 'family'), (2, 'comdey');
+
 INSERT INTO authors (id, name) VALUES (1, 'Will Smith'), (2, 'Hype Williams');
-INSERT INTO books ()
+
+INSERT INTO books (id, title, year, series_id) VALUES
+
+INSERT INTO characters (id, name, motto, species, author_id) VALUES
+
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Character One", "motto one", "cylon", 2), (6, "Character Two", "motto two", "human", 2), (7, "Character Three", "motto three", "cylon", 2), (8, "Character Four", "motto four", "cylon", 2);
+
+INSERT INTO character_books (id, book_id, character_id) VALUES
+
+INSERT INTO character_books (id, book_id, character_id) VALUES
