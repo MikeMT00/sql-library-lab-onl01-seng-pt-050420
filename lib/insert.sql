d0746480f5e68a9ab9d099190f9d1ab092428ff1
Michael Thomas <michaelthomas0611@gmail.com>
2020-07-05 11:01:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,13 @@
-INSERT INTO series (id, title) VALUES (1, 'Fresh Prince'), (2, 'Bernie Mac');
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
 
-INSERT INTO subgenres (id, name) VALUES (1, 'family'), (2, 'comdey');
+INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
 
-INSERT INTO authors (id, name) VALUES (1, 'Will Smith'), (2, 'Hype Williams');
+INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
 
-INSERT INTO books (id, title, year, series_id) VALUES (1, "Burnin' Down the House", 1996, 1), (2, "Get a Job", 1998, 1), (3, "Strees Related", 2000, 1), (4, "Not I Barbeque", 2002, 2), (5, "Viva Lost Wages", 2003, 2), (6, "There's a Rub", 2005, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
-INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Will", "What's up!", "human", 1), (2, "Carlton", "I'm a Princeton man", "human", 1), (3, "Uncle Phil", "Vivian!", "human", 1), (4, "Hilary", "Daddy!", "human", 1);
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1), (4, "Eddard Stark", "Winter is coming", "human", 1);
 
 INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Character One", "motto one", "cylon", 2), (6, "Character Two", "motto two", "human", 2), (7, "Character Three", "motto three", "cylon", 2), (8, "Character Four", "motto four", "cylon", 2);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
-
-INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
