c7f5775671851979e6db36592af78e4397b09845
Michael Thomas <michaelthomas0611@gmail.com>
2020-07-05 10:39:33 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO series (id, title) VALUES (1, 'Fresh Prince'), (2, 'Bernie Mac');
+INSERT INTO subgenres (id, name) VALUES (1, 'family'), (2, 'comdey');
+INSERT INTO authors (id, name) VALUES (1, 'Will Smith'), (2, 'Hype Williams');
+INSERT INTO books ()
