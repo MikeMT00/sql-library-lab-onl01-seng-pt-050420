786dcdbfd7584d5cc428f3cdc94673cb1dd2a5b2
Michael Thomas <michaelthomas0611@gmail.com>
2020-07-05 10:58:10 +0000

    Automatically backed up by Learn

--- lib/data.sql
+++ lib/data.sql
@@ -6,7 +6,7 @@ INSERT INTO authors (id, name) VALUES (1, "Will Smith"), (2, "Hype Williams");
 
 INSERT INTO books (id, title, year, series_id) VALUES (1, "Burnin' Down the House", 1996, 1), (2, "Get a Job", 1998, 1), (3, "Stress Related", 2000, 1), (4, "Not I Barbeque", 2002, 2), (5, "Viva Lost Wages", 2003, 2), (6, "There's the Rub", 2005, 2);
 
-INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Will", "Whats up!", "human", 1), (2, "Carlton", "I'm a Princeton man", "human", 1), (3, "Uncle Phil", "Vivian!", "human", 1), (4, "Hilary", "Daddy!", "human", 1);
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Will", "What's up!", "human", 1), (2, "Carlton", "I'm a Princeton man", "human", 1), (3, "Uncle Phil", "Vivian!", "human", 1), (4, "Hilary", "Daddy!", "human", 1);
 
 INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Character One", "motto one", "cylon", 2), (6, "Character Two", "motto two", "human", 2), (7, "Character Three", "motto three", "cylon", 2), (8, "Character Four", "motto four", "cylon", 2);
 
